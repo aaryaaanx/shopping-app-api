ddc4f3b5abd6bacf48a5a4ea3b38f94ce6253dc2
Aryan <aryansajan630@gmail.com>
2024-08-27 22:31:16 +0530

    updated commit

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,64 +1,94 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from "react";
+import NavBar from "./NavBar";
 
 const AddProduct = () => {
   return (
-   
-      
-      <div>
-            <NavBar/>
-            <div className="container">
-                <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Product Name</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Image</label>
-                            <input type="file" name="" id="image" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Description</label>
-                                <textarea name="" id="" className="form-control" placeholder="Type here... "></textarea>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Price</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Rating</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Brand</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Manufacturing Date</label>
-                                <input type="date" name="" id="" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Expiry Date</label>
-                                <input type="date" name="" id="" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Dealer Name</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button className="btn btn-danger">Submit</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div>
+      <NavBar />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Product Name
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Image
+                </label>
+                <input
+                  type="file"
+                  name=""
+                  id="image"
+                  className="form-control"
+                />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">
+                  Description
+                </label>
+                <textarea
+                  name=""
+                  id=""
+                  className="form-control"
+                  placeholder="Type here... "
+                ></textarea>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Price
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Rating
+                </label>
+                <select class="form-select" aria-label="Default select example">
+                  <option selected>Choose your Rating</option>
+                  <option value="1">1 Star</option>
+                  <option value="2">2 Star</option>
+                  <option value="3">3 Star</option>
+                  <option value="3">4 Star</option>
+                  <option value="3">5 Star</option>
+                </select>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Brand
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Manufacturing Date
+                </label>
+                <input type="date" name="" id="" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Expiry Date
+                </label>
+                <input type="date" name="" id="" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-label">
+                  Dealer Name
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-danger">Submit</button>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
 
-
-   
-  )
-}
-
-export default AddProduct
+export default AddProduct;
